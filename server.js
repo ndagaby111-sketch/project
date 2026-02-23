cb0e85cc10aa11f5280fe6de0eec3328baca85d0
ndagaby111-sketch <ndagaby111@gmail.com>
2026-02-23 10:40:48 +0530

    done

--- server.js
+++ server.js
@@ -1,32 +1,6 @@
-const express = require("express");
-const fs = require("fs");
-const path = require("path");
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("mobile-menu");
 
-const app = express();
-
-// middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-
-// contact form route
-app.post("/contact", (req, res) => {
-  const { name, email, message } = req.body;
-
-  const data = `Name: ${name} | Email: ${email} | Message: ${message}\n`;
-
-  // save to file
-  fs.appendFileSync("messages.txt", data);
-
-  res.send(`
-    <h2 style="font-family:sans-serif;text-align:center;margin-top:50px">
-    ✅ Message Sent Successfully! <br><br>
-    <a href="/">Go Back Home</a>
-    </h2>
-  `);
+btn.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
 });
-
-// start server
-app.listen(3000,"0.0.0.0", () => {
-  console.log("Server running → http://localhost:3000")
-});
- 
